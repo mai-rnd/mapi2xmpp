340dd2d53f8bdcd5a8e2dbd3a8db6248ec92b5fb
MAI <matveev.rnd@gmail.com>
2018-11-09 14:34:44 +0300

    Уменьшил потребление памяти, вроде должно меньше падать

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # mapi2xmpp
 
+В одной папке с mapi2xmpp-resend.py должен быть файл config.py со следующим содержимым:
+
 email = 'Exchange Email address' \n
 epass = 'Exchange Email pass' \n
 jid = 'Jabber Id' \n
 jid_sender = 'Sender Jabber Id' \n
 jid_sender_pass = 'Sender Jabber pass' \n
+folders = ['Support','Admin','Noreply','inbox'] \тут нужно перечислить названия папок, которые проверять нужно
--- mapi2xmpp-resend.py
+++ mapi2xmpp-resend.py
@@ -1,28 +1,56 @@
 from exchangelib import Credentials, Account
 from pyxmpp2.simple import send_message
-from config import *
+from config import email, epass, jid, jid_sender, jid_sender_pass, folders
 import time
 import html2text
 
-if __name__ == '__main__':
+def send_xmpp_msg(jid_sender, jid_sender_pass, jid, message):
+    try:
+        send_message(jid_sender, jid_sender_pass, jid, message)
+    except:
+        print("Can not send message, check something :)")
+
+def connect_mapi(email, epass):
     credentials = Credentials(email, epass)
-    account = Account(email, credentials=credentials, autodiscover=True)
+    try:
+        account = Account(email, credentials=credentials, autodiscover=True)
+        return account
+    except:
+        print("Can not connect to mail server, check something :)")
+        send_xmpp_msg(jid_sender, jid_sender_pass, jid, "Can not connect to mail server, check something :)")
+
+def check_connection(account):
+    if account:
+        pass
+    else:
+        account = connect_mapi(email, epass)
+
+def get_emassages(folder):
+    if folder in ['inbox', 'outbox', 'trash', 'sent', 'junk', 'tasks']:
+        return list(account.inbox.filter(is_read=False).all().values('subject','id','body','sender'))
+    else:
+        return list(account.root.glob('**/'+fold).filter(is_read=False).all().values('subject','id','body','sender'))
+
+if __name__ == '__main__':
+    account = connect_mapi(email, epass)
     sended = list()
+    unreaded = list()
     while True:
-        folders = ['Support','Admin','Noreply','inbox']
         for fold in folders:
-            if fold in ['inbox', 'outbox', 'trash', 'sent', 'junk', 'tasks']:
-                msg = list(account.inbox.filter(is_read=False).all().values('subject','id','body','sender'))
-            else:
-                msg = list(account.root.glob('**/'+fold).filter(is_read=False).all().values('subject','id','body','sender'))
+            msg = get_emassages(fold)
             if msg:
                 for subject in msg:
+                    unreaded.append(subject['id'])
                     if subject['id'] not in sended:
-                        send_message(jid_sender, jid_sender_pass, jid, 
-                                     '*'+fold+':*\n-----------\n'+
-                                     str(subject['sender'].email_address)+'\n-----------\n'+
-                                     subject['subject']+'\n-----------\n'+
-                                     html2text.html2text(subject['body']).replace('\n','')[:500])
+                        message = '*'+fold+':*\n-----------\n'+str(subject['sender'].email_address)+\
+                                  '\n-----------\n'+subject['subject']+'\n-----------\n'+\
+                                  html2text.html2text(subject['body']).replace('\n','')[:500]
+                        send_xmpp_msg(jid_sender, jid_sender_pass, jid, message)
                         sended.append(subject['id'])
-                msg.clear()        
+                msg.clear()
+        for iter_msg in sended:
+            if iter_msg not in unreaded:
+                sended.remove(iter_msg)  
+        unreaded.clear()    
         time.sleep(60)
+        check_connection(account)
